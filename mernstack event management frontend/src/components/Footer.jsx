1b2bd183a104d45211146e99fa0cf0756d9c3b2c
AKASH YADAV <ay708051@gmail.com>
2025-02-27 14:27:45 +0530

    Fixed input text visibility in Footer.jsx

--- mernstack event management frontend/src/components/Footer.jsx
+++ mernstack event management frontend/src/components/Footer.jsx
@@ -11,8 +11,29 @@ const Footer = () => {
         <div className="tag">
           <label>News Letter</label>
           <div>
-            <input type="text" placeholder="E-mail" />
-            <button>Signup</button>
+          <input
+  type="text"
+  placeholder="E-mail"
+  style={{
+    color: "white",
+    backgroundColor: "black",
+    border: "1px solid white",
+    padding: "8px"
+  }}
+/>
+
+<button
+  style={{
+    backgroundColor: "white",
+    color: "black",
+    border: "none",
+    padding: "8px 12px",
+    cursor: "pointer"
+  }}
+>
+  Signup
+</button>
+
           </div>
           <p>Sign up with your email address to receice news and updates!</p>
         </div>
